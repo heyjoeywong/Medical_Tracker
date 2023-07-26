8239e7797e8ca7e7409c3db4adf4c232bd7b793f
joeywong <heyjoeywong@gmail.com>
2023-07-26 16:37:49 +0800

    reformatted MedCard

--- src/meditrack/src/components/MedCard.vue
+++ src/meditrack/src/components/MedCard.vue
@@ -1,16 +1,32 @@
 <template>
     <ion-card class="ion-margin ion-padding">
         <ion-card-header>
-            <ion-card-title>Med 1</ion-card-title>
-            <ion-card-subtitle>Date Collected</ion-card-subtitle>
+            <ion-card-title>{{ }}</ion-card-title>
+            <ion-card-subtitle>Start Date: {{ }}</ion-card-subtitle>
         </ion-card-header>
 
         <ion-card-content>
-            Quantity, Schedule etc.
+            <ion-list lines="none" class="medList ion-no-padding">
+                <ion-item>
+                    <ion-label>Quantity: {{ }}</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Frequency: {{ }}</ion-label>
+                </ion-item>
+                <ion-item>
+                    <ion-label>End Date: {{ }}</ion-label>
+                </ion-item>
+            </ion-list>
         </ion-card-content>
     </ion-card>
 </template>
 
 <script setup lang="ts">
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/vue';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel } from '@ionic/vue';
 </script>
+
+<style scoped>
+.medList {
+    margin-left: -16px;
+}
+</style>
