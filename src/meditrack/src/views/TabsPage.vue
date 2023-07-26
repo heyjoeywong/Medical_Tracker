7051f7502ad52564d41f9a7679adc1fab853b6c3
joeywong <heyjoeywong@gmail.com>
2023-07-26 15:12:05 +0800

    added labels to icons

--- src/meditrack/src/views/TabsPage.vue
+++ src/meditrack/src/views/TabsPage.vue
@@ -4,15 +4,18 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="medicalOutline" aria-label="Medication" />
+          <ion-icon :icon="medicalOutline" aria-label="Medication" size="small" />
+          <ion-label>Medications</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="calendarClearOutline" aria-label="Overview" />
+          <ion-icon :icon="calendarClearOutline" aria-label="Overview" size="small" />
+          <ion-label>Overview</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="bodyOutline" aria-label="Symptoms" />
+          <ion-icon :icon="bodyOutline" aria-label="Symptoms" size="small" />
+          <ion-label>Symptoms</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -23,3 +26,5 @@
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
 import { medicalOutline, calendarClearOutline, bodyOutline } from 'ionicons/icons';
 </script>
+
+<style scoped></style>
