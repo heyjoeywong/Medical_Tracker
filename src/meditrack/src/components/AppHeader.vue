8828d7be4723a521e009dbd59885c82ee4e0b83d
joeywong <heyjoeywong@gmail.com>
2023-07-26 15:08:57 +0800

    added logo

--- src/meditrack/src/components/AppHeader.vue
+++ src/meditrack/src/components/AppHeader.vue
@@ -1,11 +1,21 @@
 <template>
-    <ion-header>
+    <ion-header class="ion-margin-bottom">
         <ion-toolbar>
-            <ion-title class="ion-text-center">Logo</ion-title>
+            <ion-img src="/resources/logo.png" class="appLogo"></ion-img>
         </ion-toolbar>
     </ion-header>
 </template>
 
 <script setup lang="ts">
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
+import { IonHeader, IonToolbar, IonTitle, IonImg } from '@ionic/vue';
 </script>
+
+<style scoped>
+.appLogo {
+    width: 80px;
+    padding: 8px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
