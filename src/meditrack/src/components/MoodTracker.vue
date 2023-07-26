b67b2ffffa39dff991c599cbabe6d474eebc0f1c
joeywong <heyjoeywong@gmail.com>
2023-07-26 15:11:44 +0800

    added mood tracker component

---
+++ src/meditrack/src/components/MoodTracker.vue
@@ -0,0 +1,13 @@
+<template>
+<ion-content>
+    <ion-title>Hi, how are you feeling today?</ion-title>
+</ion-content>
+
+</template>
+
+<script setup lang="ts">
+import { IonTitle, IonContent, IonRadioGroup, IonRadio } from '@ionic/vue';
+</script>
+
+<style scoped>
+</style>
