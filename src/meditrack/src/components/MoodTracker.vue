df3db70c9082167eea257e5b12bb10c621d70b11
joeywong <heyjoeywong@gmail.com>
2023-07-28 15:40:17 +0800

    changed radio group presentation

--- src/meditrack/src/components/MoodTracker.vue
+++ src/meditrack/src/components/MoodTracker.vue
@@ -4,8 +4,8 @@
 
         <ion-card class="ion-margin round-border">
             <form @submit.prevent="logMood">
-                <ion-card-subtitle class="ion-padding">1 = Bad, 2 = Neutral, 3 = Good</ion-card-subtitle>
-                <ion-grid class="ion-padding" :fixed="true">
+                <ion-card-subtitle class="ion-padding">Mood Tracker <br> 1 = Bad, 2 = Neutral, 3 = Good</ion-card-subtitle>
+                <!-- <ion-grid class="ion-padding" :fixed="true">
                     <ion-row class="ion-justify-content-center">
                         <ion-radio-group v-model="selectedMood">
                             <ion-col><ion-radio value="1">1</ion-radio></ion-col>
@@ -13,7 +13,16 @@
                             <ion-col><ion-radio value="3">3</ion-radio></ion-col>
                         </ion-radio-group>
                     </ion-row>
-                </ion-grid>
+                </ion-grid> -->
+
+                <ion-list>
+                    <ion-radio-group v-model="selectedMood">
+                        <ion-item><ion-radio value="1">1</ion-radio></ion-item>
+                        <ion-item><ion-radio value="2">2</ion-radio></ion-item>
+                        <ion-item><ion-radio value="3">3</ion-radio></ion-item>
+                    </ion-radio-group>
+                </ion-list>
+
                 <div class="ion-text-center ion-padding-bottom">
                     <ion-button shape="round" color="primary" type="submit">Log Mood</ion-button>
                 </div>
@@ -25,7 +34,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonTitle, IonCard, IonContent, IonGrid, IonRow, IonCol, IonRadioGroup, IonRadio, IonButton, IonCardSubtitle } from '@ionic/vue';
+import { IonTitle, IonCard, IonContent, IonGrid, IonRow, IonCol, IonRadioGroup, IonRadio, IonButton, IonCardTitle, IonCardSubtitle, IonList, IonItem } from '@ionic/vue';
 import { useStore } from 'vuex';
 import { ref } from 'vue';
 import MoodHistory from './MoodHistory.vue';
